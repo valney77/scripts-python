01f5a85b2b62436bfd8d7a441c2a99e0968a8ee2
valney77 <80773278+valney77@users.noreply.github.com>
2022-09-17 22:47:32 -0400

    Create numberlock.py

---
+++ numberlock.py
@@ -0,0 +1,25 @@
+from random import randint
+from os import system
+from time import sleep
+
+system('clear')
+
+print ('''
+____  __  ______ ___  ___  _________     ____/ /___ _
+  / __ \/ / / / __ `__ \/ _ \/ ___/ __ \   / __  / __ `/
+ / / / / /_/ / / / / / /  __/ /  / /_/ /  / /_/ / /_/ /
+/_/ /_/\__,_/_/ /_/ /_/\___/_/   \____/   \__,_/\__,_/
+   _________  _____/ /____
+  / ___/ __ \/ ___/ __/ _ l
+ (__  ) /_/ / /  / /_/  __/
+/____/\____/_/   \__/\___/\n\n''')
+numero = int(input('digite quantos numero vai precisar numero : ').center(50))
+contador = 0
+T=0
+
+for contador in range(1, numero +1):
+    T+=0
+
+    numero_sorte = randint(1,100)
+    sleep(5)
+    print ('--------------',numero_sorte,'--------------')
